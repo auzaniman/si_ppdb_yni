fb472659cff127ea5ccddf50921491231bc01c47
Auzan <auzan.ip@gmail.com>
2021-10-28 14:34:17 +0800

    first commit

---
+++ public/frontend/assets/js/app.js
@@ -0,0 +1,142 @@
+// NAVBAR FIXED TOP ON SCROLL
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 50) {
+      document.getElementById("navbar_top").classList.add("bg-nav");
+    } else {
+      document.getElementById("navbar_top").classList.remove("bg-nav");
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  function myFunction(x) {
+    if (x.matches) {
+      // If media query matches
+      document.getElementById("navbar_top").classList.remove("fixed-top");
+    } else {
+      document.getElementById("navbar_top").classList.add("fixed-top");
+    }
+  }
+
+  var x = window.matchMedia("(max-width: 700px)");
+  myFunction(x); // Call listener function at run time
+  x.addListener(myFunction); // Attach listener function on state changes
+});
+
+// MULTIDROPDOWN
+//	window.addEventListener("resize", function() {
+//		"use strict"; window.location.reload();
+//	});
+
+document.addEventListener("DOMContentLoaded", function () {
+  /////// Prevent closing from click inside dropdown
+  document.querySelectorAll(".dropdown-menu").forEach(function (element) {
+    element.addEventListener("click", function (e) {
+      e.stopPropagation();
+    });
+  });
+
+  // make it as accordion for smaller screens
+  if (window.innerWidth < 992) {
+    // close all inner dropdowns when parent is closed
+    document
+      .querySelectorAll(".navbar .dropdown")
+      .forEach(function (everydropdown) {
+        everydropdown.addEventListener("hidden.bs.dropdown", function () {
+          // after dropdown is hidden, then find all submenus
+          this.querySelectorAll(".submenu").forEach(function (everysubmenu) {
+            // hide every submenu as well
+            everysubmenu.style.display = "none";
+          });
+        });
+      });
+
+    document.querySelectorAll(".dropdown-menu a").forEach(function (element) {
+      element.addEventListener("click", function (e) {
+        let nextEl = this.nextElementSibling;
+        if (nextEl && nextEl.classList.contains("submenu")) {
+          // prevent opening link if link needs to open dropdown
+          e.preventDefault();
+          console.log(nextEl);
+          if (nextEl.style.display == "block") {
+            nextEl.style.display = "none";
+          } else {
+            nextEl.style.display = "block";
+          }
+        }
+      });
+    });
+  }
+  // end if innerWidth
+});
+// MULTIDROPDOWN END
+
+// CAROUSEL
+$(document).ready(function () {
+  $(".center").slick({
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    dots: true,
+    centerMode: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          // centerMode: true,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          dots: true,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+          infinite: true,
+          autoplay: true,
+          autoplaySpeed: 2000,
+        },
+      },
+    ],
+  });
+});
+// CAROUSEL END
+
+// TESTI
+//Testimoni
+$(document).ready(function () {
+  $(".testi").slick({
+    arrows: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    fade: true,
+    cssEase: "linear",
+  });
+});
+
+// ALUR PENDAFTARAN
+$(document).ready(function () {
+  $(".timeline-pendaftaran").slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: "linear",
+  });
+});
