238a18cb56d80f4fd49ea2efcbf8744aacaedbbb
Auzan <auzan.ip@gmail.com>
2022-01-08 08:25:32 +0700

    create statuses migration resource create cetak list blade

---
+++ resources/views/user_view/layouts/app_ppdb_checkout.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  @include('user_view.includes.meta')
+
+  <title>@yield('title')</title>
+
+  @stack('before-style')
+  {{-- style --}}
+  @include('user_view.includes.style')
+
+  @stack('after-style')
+
+</head>
+<body>
+
+  @include('user_view.includes.navbar_ppdb')
+  @yield('content')
+  @include('user_view.includes.footer')
+
+  @stack('before-script')
+  {{-- script --}}
+  @include('user_view.includes.script')
+
+  @stack('after-script')
+
+</body>
+</html>
