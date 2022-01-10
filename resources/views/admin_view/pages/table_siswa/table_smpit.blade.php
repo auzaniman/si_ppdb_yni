b9668ed615fe5b44de52996b4a94e8ca7957cd8d
Auzan <auzan.ip@gmail.com>
2022-01-10 08:06:02 +0700

    add function download export to excel and word

--- resources/views/admin_view/pages/table_siswa/table_smpit.blade.php
+++ resources/views/admin_view/pages/table_siswa/table_smpit.blade.php
@@ -6,12 +6,24 @@ Table Data
 
 @section('content')
 <div class="container-fluid">
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <script>
+      function exportF(elem) {
+          var table = document.getElementById("dataTable");
+          var html = table.outerHTML;
+          var url = 'data:application/vnd.ms-excel,' + escape(html); // Set your html table into url
+          elem.setAttribute("href", url);
+          elem.setAttribute("download", "Data Peserta.xls"); // Choose the file name
+          return false;
+      }
+  </script>
+  <h5 class="alert alert-info">Mohon Tunggu Hingga Data Muncul di Semua Field, Kemudian Klik Export to Excel</h5>
+
   <!-- Page Heading -->
-  <h1 class="h3 mb-2 text-gray-800">Tables</h1>
-  <p class="mb-4">
-    DataTables is a third party plugin that is used to generate the
-    demo table below. For more information about DataTables
-  </p>
+  <div class="row justify-content-between mx-0 mb-4">
+    <h1 class="h3 text-gray-800">Data Peserta PPDB SMPIT</h1>
+    <a id="downloadLink" class="btn btn-success text-decoration-none" onclick="exportF(this)">Export to excel</a>
+  </div>
 
   <!-- DataTales Example -->
   <div class="card shadow mb-4">
@@ -26,21 +38,36 @@ Table Data
           class="table table-bordered"
           id="dataTable"
           width="100%"
+          border="1"
           cellspacing="0"
+          cellpadding="5"
         >
-          <thead>
+          <thead class="text-center">
             <tr>
-              <th>Nama Siswa</th>
-              <th>TTL</th>
-              <th>NIK/NISN</th>
-              <th>Asal Sekolah</th>
-              <th>Alamat</th>
-              <th>Nama Ayah/Pekerjaan/NIK</th>
-              <th>Nama Ibu/Pekerjaan/NIK</th>
-              <th>HP Siswa/Ortu</th>
-              <th>Program</th>
-              <th>File</th>
-              <th>Status</th>
+              <th rowspan="2">Nama Siswa</th>
+              <th rowspan="2">TTL</th>
+              <th colspan="3">No Induk</th>
+              <th rowspan="2">Asal Sekolah</th>
+              <th rowspan="2">Alamat Tinggal</th>
+              <th colspan="3">Identitas Ayah</th>
+              <th colspan="3">Identitsan Ibu</th>
+              <th colspan="2">Nomor HP</th>
+              <th rowspan="2">Program</th>
+              <th rowspan="2">File</th>
+              <th rowspan="2">Status</th>
+            </tr>
+            <tr>
+              <th>NIK</th>
+              <th>NISN</th>
+              <th>KK</th>
+              <th>Nama</th>
+              <th>NIK</th>
+              <th>Pekerjaan</th>
+              <th>Nama</th>
+              <th>NIK</th>
+              <th>Pekerjaan</th>
+              <th>Siswa</th>
+              <th>Ortu</th>
             </tr>
           </thead>
           <tbody>
@@ -49,12 +76,19 @@ Table Data
             <tr>
               <td>{{ $smp->nama }}</td>
               <td>{{ $smp->ttl }}</td>
-              <td>{{ $smp->nik }}/{{ $smp->nisn }}/{{ $smp->kk }}</td>
+              <td>{{ $smp->nik }}</td>
+              <td>{{ $smp->nisn }}</td>
+              <td>{{ $smp->kk }}</td>
               <td>{{ $smp->asal_sekolah }}</td>
               <td>{{ $smp->alamat }}</td>
-              <td>{{ $smp->nama_ayah }}/{{ $smp->pekerjaan_ayah }}/{{ $smp->nik_ayah }}</td>
-              <td>{{ $smp->nama_ibu }}/{{ $smp->pekerjaan_ibu }}/{{ $smp->nik_ibu }}</td>
-              <td>{{ $smp->hp_siswa }}/{{ $smp->hp_ortu }}</td>
+              <td>{{ $smp->nama_ayah }}</td>
+              <td>{{ $smp->nik_ayah }}</td>
+              <td>{{ $smp->pekerjaan_ayah }}</td>
+              <td>{{ $smp->nama_ibu }}</td>
+              <td>{{ $smp->nik_ibu }}</td>
+              <td>{{ $smp->pekerjaan_ibu }}</td>
+              <td>{{ $smp->hp_siswa }}</td>
+              <td>{{ $smp->hp_ortu }}</td>
               <td>{{ $smp->program }}</td>
               <td>
                 <img src="{{ asset('storage/assets/ppdb/'.$smp->image) }}" alt="" style="width: 150px" class="img-thumbnail">
--- resources/views/user_view/pages/cetak_kartu/cetak.blade.php
+++ resources/views/user_view/pages/cetak_kartu/cetak.blade.php
@@ -5,12 +5,44 @@ List Pendaftar
 @endsection
 
 @section('content')
+
+
 <section>
   <div class="container mt-5">
-    <h3>Silahkan Cetak Kartu Peserta</h3>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <script>
+      function exportF(elem) {
+          var table = document.getElementById("cetak");
+          var html = table.outerHTML;
+          var url = 'data:application/vnd.ms-word,' + escape(html); // Set your html table into url
+          elem.setAttribute("href", url);
+          elem.setAttribute("download", "Kartu Peserta.doc"); // Choose the file name
+          return false;
+      }
+  </script>
 
-    <div class="table-responsive">
-      <table class="table table-bordered">
+  <h5 class="alert alert-info">Mohon Tunggu Hingga Data Muncul di Semua Field, Kemudian Klik Unduh ke Word</h5>
+
+  <div class="mx-0 mb-5">
+    <h3>Silahkan Cetak Kartu Peserta</h3>
+    <a id="downloadLink" class="btn btn-primary text-decoration-none" onclick="exportF(this)">Unduh ke Word</a>
+  </div>
+
+    <div class="table-responsive" id="cetak">
+      <div class="row mb-3">
+        <div class="col-3 text-center">
+          <img width="100" height="100" class="img-fluid" src="{{ url ('frontend/assets/img/YNI/SMPIT/logo.png')}}" alt="logo SMPIT">
+        </div>
+        <div class="col-6 text-center">
+          <h4>KARTU BUKTI PENDAFTARAN CALON SISWA/SISWI BARU <br>
+          SEKOLAH ISLAM TERPADU NURUL 'ILMI TENGGARONG <br>
+        SMPIT TAHUN 2021/2022</h4>
+        </div>
+        <div class="col-3">
+          <h4>Keterangan</h4>
+        </div>
+      </div>
+      <table border="1" cellpadding="5" class="table table-bordered">
         <tbody>
           <tr>
             <td>Nama</td>
